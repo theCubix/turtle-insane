d16eaba976105bfd309c19437c5ed541d7e749b2
[thecubix] <[timon.forrer@gmail.com]>
2019-02-25 21:40:13 +0100

    minor fix (margin top on container)

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <div class="container">
+    <div class="container has-margin-top">
       <h1>About us</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
     </div>
